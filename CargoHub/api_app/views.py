b94b389d1b5a74347b0c17aaa01905bd1f700d24
Charlie <1021342@hr.nl>
2024-12-03 11:58:46 +0100

    Wrm werkt het niet meer

--- CargoHub/api_app/views.py
+++ CargoHub/api_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.conf import settings
 # Create your views here.
+# test
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
