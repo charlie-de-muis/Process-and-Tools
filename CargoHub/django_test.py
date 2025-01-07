2b95522836c8ff81e535fa2b1c6220c65101bcd4
charlie-de-muis <tiffanyvh37@gmail.com>
2025-01-07 13:43:57 +0100

    modified:   CargoHub/api/.rest 	modified:   CargoHub/django_test.py

--- CargoHub/django_test.py
+++ CargoHub/django_test.py
@@ -2,7 +2,7 @@ import pytest
 from django.urls import reverse
 from rest_framework import status
 import requests
-
+#please
 # HI I DID IT?????
 @pytest.mark.django_db
 def test_api_app_status(client):
